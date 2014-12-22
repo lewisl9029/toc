2f3bd6590b10199faf4419ce4cfc38ab30d891db
Lewis Liu <develop@lewisl.net>
2014-12-21 22:14:07 -0800

    Moved aliases to provisioning script and stored permanently in .bash_aliases

--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -2,3 +2,11 @@
 apt-get update
 apt-get -y install docker.io dos2unix
 apt-get clean
+
+(
+cat <<EOF
+alias toc="sudo /vagrant/docker-run.sh"
+alias btoc="sudo /vagrant/docker-build.sh"
+alias ptoc="sudo /vagrant/docker-push.sh"
+EOF
+) >> ~/.bash_aliases
