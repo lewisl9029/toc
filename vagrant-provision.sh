00249bf798a0f908ccb38097714f37661bd70d21
Lewis Liu <develop@lewisl.net>
2014-12-22 01:45:49 -0500

    Fixed aliase, adding line ending conversion step to vagrant provisioning

--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -3,10 +3,14 @@ apt-get update
 apt-get -y install docker.io dos2unix
 apt-get clean
 
+dos2unix /vagrant/docker-run.sh
+dos2unix /vagrant/docker-build.sh
+dos2unix /vagrant/docker-push.sh
+
 (
 cat <<EOF
 alias toc="sudo /vagrant/docker-run.sh"
 alias btoc="sudo /vagrant/docker-build.sh"
 alias ptoc="sudo /vagrant/docker-push.sh"
 EOF
-) >> ~/.bash_aliases
+) >> .bash_aliases
