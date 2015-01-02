839f5e8302c78f1542d4ea9fde88f1e731af94ac
Lewis Liu <develop@lewisl.net>
2015-01-01 22:57:47 -0500

    Switched npm caching container to use npm_lazy

--- Vagrantfile
+++ Vagrantfile
@@ -9,10 +9,12 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   config.vm.network "forwarded_port", guest: 8100, host: 8100
   config.vm.network "forwarded_port", guest: 8101, host: 8101
+  config.vm.network "forwarded_port", guest: 8201, host: 8201
+  config.vm.network "forwarded_port", guest: 8202, host: 8202
   config.vm.network "forwarded_port", guest: 35729, host: 35729
 
   config.vm.synced_folder ".", "/toc"
-  config.vm.synced_folder "./cache", "/var/cache/toc"
+  #config.vm.synced_folder "./cache", "/var/cache/toc"
   config.vm.synced_folder ".", "/vagrant", disabled: true
 
   config.vm.provider "virtualbox" do |v|
--- containers/toc-setup-env.sh
+++ containers/toc-setup-env.sh
@@ -3,11 +3,11 @@ TOC_VER="$(cat $TOC_DIR/containers/toc.version)"
 
 sudo docker build -t toc-cache-apt:$TOC_VER $TOC_DIR/containers/cache/apt
 sudo docker build -t toc-cache-apt:latest $TOC_DIR/containers/cache/apt
-sudo docker run -d -p 3142:3142 -v /var/cache/toc/apt:/var/cache/apt-cacher-ng toc-cache-apt:latest
+sudo docker run --name toc-cache-apt -d -p 8201:3142 -v /var/cache/toc/apt:/var/cache/apt-cacher-ng toc-cache-apt:latest
 
 sudo docker build -t toc-env:$TOC_VER $TOC_DIR/containers/env
 sudo docker build -t toc-env:latest $TOC_DIR/containers/env
 
 sudo docker build -t toc-cache-npm:$TOC_VER $TOC_DIR/containers/cache/npm
 sudo docker build -t toc-cache-npm:latest $TOC_DIR/containers/cache/npm
-sudo docker run -d -p 4873:4873 -v /var/cache/toc/npm:/opt/sinopia/storage toc-cache-npm:latest
+sudo docker run --name toc-cache-npm -d -p 8202:8080 -v /var/cache/toc/npm:/root/.npm_lazy toc-cache-npm:latest
