22e8c577e45aee28b511c4cc1cc8a25a3373ead9
Lewis Liu <develop@lewisl.net>
2015-01-17 05:19:45 -0500

    Added html linting & rules, optimized script loading order, added initial delay to spinner

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var jshint = require('gulp-jshint');
+var htmlhint = require('gulp-htmlhint');
 var replace = require('gulp-replace');
 var rename = require('gulp-rename');
 var karma = require('karma').server;
@@ -21,6 +22,11 @@ var paths = {
     './www/views/**/*.js',
     './www/*.js',
     './*.js'
+  ],
+  html: [
+    './www/components/**/*.html',
+    './www/views/**/*.html',
+    './www/*.html'
   ]
 };
 
@@ -39,7 +45,7 @@ gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
 });
 
-gulp.task('test', ['verify'], function(done) {
+gulp.task('test', ['lint'], function(done) {
   gulp.src('./www/config.js')
     .pipe(replace('jspm_packages', 'www/jspm_packages'))
     .pipe(rename('config-test.js'))
@@ -51,11 +57,18 @@ gulp.task('test', ['verify'], function(done) {
   }, argv.prod ? done : undefined);
 });
 
-gulp.task('verify', function() {
+gulp.task('lint', function() {
   gulp.src(paths.js)
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(jshint.reporter('fail'));
+
+  gulp.src(paths.html)
+    .pipe(htmlhint({
+      htmlhintrc: './.htmlhintrc'
+    }))
+    .pipe(htmlhint.reporter())
+    .pipe(htmlhint.failReporter());
 });
 
 gulp.task('style', function() {
