6b77258ecf94c3b00f28b7118cdf0517dc0f8425
Lewis Liu <develop@lewisl.net>
2015-01-16 22:29:44 -0500

    Added splash screen overlay and css animations

--- www/initialize.js
+++ www/initialize.js
@@ -1,9 +1,10 @@
+import angular from 'angular';
 import app from './app';
 
 let initialize = () => {
   angular.element(document).ready(function() {
     angular.bootstrap(document.querySelector('[data-toc-app]'), [
-    app.name
+      app.name
     ]);
   });
 };
