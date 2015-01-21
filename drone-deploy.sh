f714b21957bd82e77dde3646fd4d79bf92f36c51
Lewis Liu <develop@lewisl.net>
2015-01-20 21:45:19 -0500

    Fixed commit message always using branch name

--- drone-deploy.sh
+++ drone-deploy.sh
@@ -12,12 +12,14 @@ cd /toc-staging/
 
 if [ "$DRONE_BRANCH" == "master" ];
 then
+  DEPLOYMENT_PATH=http://toc-staging.azurewebsites.net/
   cp -Rf $DRONE_BUILD_DIR/prod/* /toc-staging/
 else
+  DEPLOYMENT_PATH=http://toc-staging.azurewebsites.net/dev/$DRONE_BRANCH/www/
   mkdir -p /toc-staging/dev/$DRONE_BRANCH/
   cp -Rf $DRONE_BUILD_DIR/prod/* /toc-staging/dev/$DRONE_BRANCH/
 fi
 
 git add -A .
-git commit -m "Staging Toc $TOC_VER for branch $DRONE_BRANCH at http://toc-staging.azurewebsites.net/dev/$DRONE_BRANCH/www/" --allow-empty
+git commit -m "Staging Toc $TOC_VER at $DEPLOYMENT_PATH" --allow-empty
 git push origin master
