32dbe0032cbedb690b4f988f88c910d821ba0dbd
Lewis Liu <develop@lewisl.net>
2015-01-31 20:51:28 -0500

    Relaxed timings some more

--- karma.conf.js
+++ karma.conf.js
@@ -105,6 +105,6 @@ module.exports = function(config) {
     //TODO: run tests on a faster machine
     // see https://github.com/karma-runner/karma/issues/598
     captureTimeout: 60000,
-    browserNoActivityTimeout: 30000
+    browserNoActivityTimeout: 60000
   });
 };
