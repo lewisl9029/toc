ae16b9a7a20ded1673fc2319d955e0505687f3d3
Lewis Liu <develop@lewisl.net>
2015-01-31 23:12:46 -0500

    Fixed wrong ionic version

--- www/config.js
+++ www/config.js
@@ -14,7 +14,7 @@ System.config({
     "css": "github:systemjs/plugin-css@0.1.0",
     "es6-shim": "github:paulmillr/es6-shim@0.22.1",
     "html": "github:systemjs/plugin-text@0.0.2",
-    "ionic": "github:driftyco/ionic-bower@0.9.26",
+    "ionic": "github:driftyco/ionic-bower@1.0.0-beta.14",
     "plugin-text": "github:systemjs/plugin-text@0.0.2",
     "ramda": "github:ramda/ramda@0.8.0",
     "remotestorage.js": "github:remotestorage/remotestorage.js@0.11.1",
@@ -27,6 +27,12 @@ System.config({
       "angular": "github:angular/bower-angular@1.3.11"
     },
     "github:driftyco/ionic-bower@0.9.26": {
+      "angular": "github:angular/bower-angular@1.3.11",
+      "angular-animate": "github:angular/bower-angular-animate@1.3.11",
+      "angular-sanitize": "github:angular/bower-angular-sanitize@1.3.11",
+      "angular-ui-router": "github:angular-ui/ui-router@0.2.10"
+    },
+    "github:driftyco/ionic-bower@1.0.0-beta.14": {
       "angular": "github:angular/bower-angular@1.3.11",
       "angular-animate": "github:angular/bower-angular-animate@1.3.11",
       "angular-sanitize": "github:angular/bower-angular-sanitize@1.3.11",
---
+++ www/jspm_packages/github/driftyco/ionic-bower@1.0.0-beta.14.js
@@ -0,0 +1,1 @@
+module.exports = require("github:driftyco/ionic-bower@1.0.0-beta.14/js/ionic-angular");
