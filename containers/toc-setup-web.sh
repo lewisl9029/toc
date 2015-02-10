f9ac93c960cb65d5bcb6908e12e206bcbec51c39
Lewis Liu <develop@lewisl.net>
2015-02-09 22:17:35 -0500

    Inverted build and template dockerfiles

--- containers/toc-setup-web.sh
+++ containers/toc-setup-web.sh
@@ -2,27 +2,27 @@
 TOC_VER="$(git -C $TOC_DIR describe --tags --abbrev=0)"
 
 TOC_NODE_PACKAGE_NAME="node-v0.12.0-linux-x64.tar.gz"
-if [ ! -f $TOC_DIR/containers/dev/.packages/$TOC_NODE_PACKAGE_NAME ];
+if [ ! -f $TOC_DIR/containers/dev/.build/$TOC_NODE_PACKAGE_NAME ];
   then
   curl https://dl.dropboxusercontent.com/u/172349/$TOC_NODE_PACKAGE_NAME \
-  --create-dirs \
-  -o $TOC_DIR/containers/dev/.packages/$TOC_NODE_PACKAGE_NAME
+  --create-dirs --progress-bar \
+  -o $TOC_DIR/containers/dev/.build/$TOC_NODE_PACKAGE_NAME
 fi
 
 TOC_CHROME_PACKAGE_NAME="google-chrome-stable_current_amd64_v20150209.deb"
-if [ ! -f $TOC_DIR/containers/dev/.packages/$TOC_CHROME_PACKAGE_NAME ];
+if [ ! -f $TOC_DIR/containers/dev/.build/$TOC_CHROME_PACKAGE_NAME ];
   then
   curl https://dl.dropboxusercontent.com/u/172349/$TOC_CHROME_PACKAGE_NAME \
-  --create-dirs \
-  -o $TOC_DIR/containers/dev/.packages/$TOC_CHROME_PACKAGE_NAME
+  --create-dirs --progress-bar \
+  -o $TOC_DIR/containers/dev/.build/$TOC_CHROME_PACKAGE_NAME
 fi
 
 sed "s/{{TOC_NODE_PACKAGE_NAME}}/$TOC_NODE_PACKAGE_NAME/g; \
   s/{{TOC_CHROME_PACKAGE_NAME}}/$TOC_CHROME_PACKAGE_NAME/g" \
-  $TOC_DIR/containers/dev/template/Dockerfile \
-  | tee $TOC_DIR/containers/dev/Dockerfile
+  $TOC_DIR/containers/dev/Dockerfile \
+  | tee $TOC_DIR/containers/dev/.build/Dockerfile
 
 sudo docker build \
-  -t toc-dev:$TOC_VER $TOC_DIR/containers/dev
+  -t toc-dev:$TOC_VER $TOC_DIR/containers/dev/.build
 sudo docker build \
-  -t toc-dev:latest $TOC_DIR/containers/dev
+  -t toc-dev:latest $TOC_DIR/containers/dev/.build
