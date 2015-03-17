705762c6149cbf7f4c84d4a1b40b46f4b652a05a
Lewis Liu <develop@lewisl.net>
2015-03-17 05:06:09 -0400

    Implemented listening function

--- app/services/network/network-service.js
+++ app/services/network/network-service.js
@@ -1,4 +1,4 @@
-export default function network($q, state, telehash) {
+export default function network($q, $log, state, telehash) {
   const NETWORK_PATH = ['network'];
   const NETWORK_CURSORS = {
     synchronized: state.synchronized.tree.select(NETWORK_PATH)
@@ -40,6 +40,35 @@ export default function network($q, state, telehash) {
       return sessionInfo;
     });
   };
+
+  let handleMessage = function handleMessage(error, packet, channel, callback) {
+    if (error) {
+      return $log.error(error);
+    }
+
+    //FIXME: get date from packet instead
+    let message = {
+      id: Date.now().toString(),
+      content: packet.js.m
+    };
+
+    let messageCursor = state.synchronized.tree
+      .select(['messages', message.id]);
+
+    //TODO: implement toast on new message arrival
+    state.save(messageCursor, message)
+      .catch($log.error);
+  };
+  //TODO: implement channel creation as follows:
+  // direct message channel id = sorted(sender, receiver)
+  // group message channel id = channelname-creatorhashname
+  let listen = function listen(channelName, handlePacket,
+    session = activeSession) {
+    checkSession(session);
+
+    session.listen(channelName, handlePacket);
+  };
+
 
   return {
     NETWORK_CURSORS,
