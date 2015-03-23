352ed8e4dba8088c8e88e9a5b14c65b24b2f1430
Lewis Liu <develop@lewisl.net>
2015-03-23 09:20:02 -0400

    Implemented new loading screen styling

--- app/initialize.js
+++ app/initialize.js
@@ -1,21 +1,34 @@
 (function initialize() {
   //TODO: optimize spinner removal timing UX
   // Record time here
-  System.import('app')
+  var loadingIndicator;
+  var loadingStartTime;
+  var queryAnimationTime = 2800;
+
+  var loadingScreen = document.getElementsByClassName('loading-screen')[0];
+
+
+  System.import('mprogress')
+    .then(function initializeLoadingIndicator() {
+      loadingIndicator = new Mprogress({template: 4});
+      loadingStartTime = Date.now();
+      loadingIndicator.start();
+
+      return System.import('app');
+    })
     .then(function initializeApp(app) {
       app.initialize();
     })
-    .then(function hideSplashscreen() {
-      // Remove if time elapsed < 1s
-      // Else fadeout
-      // var spinner = document.getElementsByClassName('spinner')[0];
-      // spinner.className += ' fadeout-spinner';
+    .then(function hideLoadingScreen() {
+      // loadingIndicator.set(0.99);
+      var loadingEndTime = Date.now();
+      var queryElapsedTime =
+        (loadingEndTime - loadingStartTime) % queryAnimationTime;
 
-      var splashscreen = document.getElementsByClassName('splashscreen')[0];
-      splashscreen.className += ' fadeout-splashscreen';
-      setTimeout(function removeSplashscreen() {
-        splashscreen.parentNode.removeChild(splashscreen);
-      }, 1000);
+      setTimeout(function() {
+        loadingIndicator.end();
+        loadingScreen.className += ' fadeout-loading-screen';
+      }, queryAnimationTime - queryElapsedTime);
     })
     .catch(console.onerror);
 })();
