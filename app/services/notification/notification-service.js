6d15ea880dff6c7754bdf61f1a3876fabbe31792
Lewis Liu <develop@lewisl.net>
2015-03-24 05:48:55 -0400

    Cherry picked some of the changes from other branch

--- app/services/notification/notification-service.js
+++ app/services/notification/notification-service.js
@@ -1,3 +1,13 @@
-export default function notification(angularToastr) {
-  return angularToastr;
+export default function notification($q, angularToastr) {
+  let createToastrAsync = function createToastrPromise(toastrFunction) {
+    return (message, title) =>
+      $q.when(toastrFunction(message.message || message, title));
+  };
+
+  return {
+    error: createToastrAsync(angularToastr.error),
+    success: createToastrAsync(angularToastr.success),
+    info: createToastrAsync(angularToastr.info),
+    warning: createToastrAsync(angularToastr.warning),
+  };
 }
