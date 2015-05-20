0ef02c1eaae47d6bdfd8fb5cbccac8b33fe007f5
Lewis Liu <develop@lewisl.net>
2015-05-20 00:31:20 -0700

    Implemented logout functionality
    Just reloads the page. Will have to do for now.

--- app/views/home/home-controller.js
+++ app/views/home/home-controller.js
@@ -1,4 +1,5 @@
-export default function HomeController(state, identity, network) {
+export default function HomeController(state, identity, network, notification,
+  $ionicPopup, $q) {
   let currentUserCursor = state.synchronized.cursors.identity;
 
   let activeChannelId = state.synchronized.cursors.network.get(
@@ -12,6 +13,28 @@ export default function HomeController(state, identity, network) {
       'home'
     );
   }
+
+  this.showSignoutConfirm = function showSignoutConfirm() {
+    let signoutPopup = $ionicPopup.confirm({
+      title: 'Sign Out',
+      template: 'Are you sure?',
+      okText: 'Sign out',
+      okType: 'button-assertive button-outline'
+    });
+
+    signoutPopup.then((response) => {
+      if (!response) {
+        return;
+      }
+
+      return state.remove(
+          state.persistent.cursors.identity,
+          [currentUserCursor.get(['userInfo', 'id']), 'savedCredentials']
+        )
+        .then(() => $q.when(window.location.reload()))
+        .catch((error) => notification.error(error, 'Signout Error'));
+    });
+  };
 
   this.currentUser = currentUserCursor.get();
 
