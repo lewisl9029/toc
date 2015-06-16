b2bfc077eb85c1b745fae34b128a36de91648e3f
Lewis Liu <develop@lewisl.net>
2015-06-16 15:24:43 -0700

    Spun off landing page to toc-landing repo
    Fixed gulp sass task ending watch stream

--- app/initialize.js
+++ app/initialize.js
@@ -1,31 +1,31 @@
 (function initialize() {
-  // //TODO: optimize spinner removal timing UX
-  // // Record time here
-  // var loadingIndicator;
-  // var loadingStartTime;
-  // var QUERY_DURATION = 2800;
-  // var FADEOUT_DURATION = 1500;
-  //
-  // var loadingScreen = document.getElementsByClassName('toc-loading-screen')[0];
-  // loadingStartTime = Date.now();
-  //
-  // System.import('app')
-  //   .then(function initializeApp(app) {
-  //     app.initialize();
-  //   })
-  //   .then(function hideLoadingScreen() {
-  //     // loadingIndicator.set(0.99);
-  //     var loadingEndTime = Date.now();
-  //     var queryElapsedTime =
-  //       (loadingEndTime - loadingStartTime) % QUERY_DURATION;
-  //
-  //     setTimeout(function() {
-  //       loadingScreen.className += ' toc-fadeout-loading-screen';
-  //
-  //       setTimeout(function() {
-  //         loadingScreen.className += ' toc-non-interactive';
-  //       }, FADEOUT_DURATION);
-  //     }, QUERY_DURATION - queryElapsedTime);
-  //   })
-  //   .catch(console.onerror);
+  //TODO: optimize spinner removal timing UX
+  // Record time here
+  var loadingIndicator;
+  var loadingStartTime;
+  var QUERY_DURATION = 2800;
+  var FADEOUT_DURATION = 1500;
+
+  var loadingScreen = document.getElementsByClassName('toc-loading-screen')[0];
+  loadingStartTime = Date.now();
+
+  System.import('app')
+    .then(function initializeApp(app) {
+      app.initialize();
+    })
+    .then(function hideLoadingScreen() {
+      // loadingIndicator.set(0.99);
+      var loadingEndTime = Date.now();
+      var queryElapsedTime =
+        (loadingEndTime - loadingStartTime) % QUERY_DURATION;
+
+      setTimeout(function() {
+        loadingScreen.className += ' toc-fadeout-loading-screen';
+
+        setTimeout(function() {
+          loadingScreen.className += ' toc-non-interactive';
+        }, FADEOUT_DURATION);
+      }, QUERY_DURATION - queryElapsedTime);
+    })
+    .catch(console.onerror);
 })();
