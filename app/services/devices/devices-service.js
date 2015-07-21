c3892750ecc813fee74596eb4f9146b5bc99deb5
Lewis Liu <develop@lewisl.net>
2015-07-21 03:36:40 -0700

    Added remote login prompt
    Allows cancelling of auto disconnect

--- app/services/devices/devices-service.js
+++ app/services/devices/devices-service.js
@@ -1,5 +1,5 @@
 export default function devices(state, session, cryptography, R, $q, $log,
-  $interval, $timeout) {
+  $interval, $timeout, $ionicPopup) {
   let updateKillFlags = function updateKillFlags() {
     let localDeviceId = state.local.devices.get(['deviceInfo', 'id']);
 
@@ -28,16 +28,31 @@ export default function devices(state, session, cryptography, R, $q, $log,
         return;
       }
 
-      $log.info('killing current device in 10s');
+      let killingDevice = $timeout(() => {
+        session.signOut();
+      }, 5000);
 
-      $timeout(() => {
-        $log.info('killing current device');
-      }, 10000);
+      let remoteLoginPopup = $ionicPopup.show({
+        title: 'Another device has connected',
+        template: 'This device will disconnect in 5 seconds.',
+        buttons: [
+          {
+            text: 'Stay connected',
+            type: 'button-assertive',
+            onTap: (event) => {
+              $timeout.cancel(killingDevice);
+              updateKillFlags();
+            }
+          }
+        ]
+      });
     };
 
     state.addListener(localKillFlagCursor, handleKillFlagUpdate, null, {
       skipInitialize: true
     });
+
+    return $q.when();
   };
 
   let createDeviceId = function createDeviceId() {
