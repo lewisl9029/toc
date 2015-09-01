56d51f2e7bc54db94546789771dda4298b93f546
Lewis Liu <develop@lewisl.net>
2015-09-01 00:43:11 -0700

    Added saving message to buffer

--- app/services/messages/messages-service.js
+++ app/services/messages/messages-service.js
@@ -1,14 +1,60 @@
 export let serviceName = 'messages';
 export default /*@ngInject*/ function messages(
-  state
+  $q,
+  network,
+  time,
+  state,
+  R
 ) {
   let saveReceivedMessage = saveReceivedMessage() {
 
   };
 
-  let saveSentMessage = saveSentMessage(channelId, messageContent) {
-    return state.save()
+  let saveSendingMessage = saveSendingMessage(channelId, messageContent) {
+    if (!messageContent) {
+      return $q.reject('messages: message must not be empty');
+    }
+
+    let userId = state.cloud.identity.get(['userInfo', 'id']);
+    let logicalClock = state.cloud.channels.get([channelId, 'logicalClock']);
+    let newLogicalClock = logicalClock + 1;
+    let sentTime = time.getTime();
+    let contactIds = state.cloud.channels.get([channelId, 'contactIds']);
+
+    let messageId = `${sentTime}-${userId}`;
+
+    let messageInfo = {
+      id: messageId,
+      senderId: userId,
+      sentTime: sentTime,
+      logicalClock: newLogicalClock,
+      content: messageContent
+    };
+
+    let bumpLogicalClock = () => state.save(
+      state.cloud.channels,
+      [channelId, 'logicalClock'],
+      newLogicalClock
+    );
+
+    let saveMessageInfo = () => state.save(
+      state.cloud.messages,
+      [channelId, 'messageInfo'],
+      messageInfo
+    );
+
+    let addToMessageBuffer = () => $q.all(R.map((contactId) => state.save(
+      state.cloud.buffer,
+      ['messages', contactId, messageId],
+      { messageId, channelId, contactId }
+    ))(contactIds));
+
+    return bumpLogicalClock()
+      .then(saveMessageInfo)
+      .then(addToMessageBuffer);
   };
 
-  return {};
+  return {
+    saveSendingMessage
+  };
 }
