286204e0d2af6fa650aa05204eb95b16828deaaa
Lewis Liu <develop@lewisl.net>
2015-09-07 07:36:53 -0700

    Added even older versions

---
+++ releases/v0.1.0-alpha/initialize.js
@@ -0,0 +1,34 @@
+(function initialize() {
+  //TODO: optimize spinner removal timing UX
+  // Record time here
+  var loadingIndicator;
+  var loadingStartTime;
+  var QUERY_DURATION = 2800;
+  var FADEOUT_DURATION = 1500;
+
+  var loadingScreen = document.getElementsByClassName('toc-loading-screen')[0];
+  loadingIndicator = new Mprogress({template: 4});
+  loadingStartTime = Date.now();
+  loadingIndicator.start();
+
+  System.import('app')
+    .then(function initializeApp(app) {
+      app.initialize();
+    })
+    .then(function hideLoadingScreen() {
+      // loadingIndicator.set(0.99);
+      var loadingEndTime = Date.now();
+      var queryElapsedTime =
+        (loadingEndTime - loadingStartTime) % QUERY_DURATION;
+
+      setTimeout(function() {
+        loadingIndicator.end();
+        loadingScreen.className += ' toc-fadeout-loading-screen';
+
+        setTimeout(function() {
+          loadingScreen.className += ' toc-non-interactive';
+        }, FADEOUT_DURATION);
+      }, QUERY_DURATION - queryElapsedTime);
+    })
+    .catch(console.onerror);
+})();
