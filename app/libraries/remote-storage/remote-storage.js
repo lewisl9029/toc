20eddaea35ce8fbe0f0f5ad4337687a0d045f472
Lewis Liu <develop@lewisl.net>
2015-09-07 20:08:47 -0700

    Added custom forks for ionic and ionic-angular
    Added descriptions for forks used in project

--- app/libraries/remote-storage/remote-storage.js
+++ app/libraries/remote-storage/remote-storage.js
@@ -1,5 +1,7 @@
 import angular from 'angular';
 
+// custom fork of remoteStorage v0.11.2 with cordova oauth support
+// TODO: remoteStorage v0.12.x should obsolete this, migrate when available
 import remoteStorage from './remote-storage-library';
 
 export default angular.module('toc.libraries.remote-storage', [])
--- app/libraries/telehash/telehash.js
+++ app/libraries/telehash/telehash.js
@@ -1,5 +1,8 @@
 import angular from 'angular';
 
+// custom browserify build of telehash v2 that includes all dependencies
+// needed because some deps pointed to repositories that are no longer available
+//TODO: keep up with telehash v3 development and blockname
 import th from './telehash-library';
 
 export default angular.module('toc.libraries.telehash', [])
