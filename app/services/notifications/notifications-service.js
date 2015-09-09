88d2148dd7b1966153357695b6d9c43789989492
Lewis Liu <develop@lewisl.net>
2015-09-09 02:53:46 -0700

    Added numeric notification id required by cordova
    Added some very lax csp rules to stop cordova complaints
    Removed depcache step from packaging

--- app/services/notifications/notifications-service.js
+++ app/services/notifications/notifications-service.js
@@ -1,13 +1,26 @@
 export let serviceName = 'notifications';
 export default /*@ngInject*/ function notifications(
+  $cordovaLocalNotification,
   $q,
   state
 ) {
+  // cordovaLocalNotification uses number IDs
+  // notificationId is just channelId, and has the form toc-{32hex-chars}
+  // we translate channelId to cordovaNotificationId by parsing the last 8 hex
+  // because js ints lack the precision to represent more than 52 bits
+  // FIXME: there is a non-negligible chance for collisions here
+  // but I can't think of any good alternatives atm
+  let getCordovaNotificationId = (notificationId) => {
+    let lastEightHex = notificationId.substr(notificationId.length - 8);
+    return parseInt(lastEightHex, 16);
+  };
+
   let notify = function notify(notificationId) {
+    let cordovaId = getCordovaNotificationId(notificationId);
     return state.save(
         state.cloud.notifications,
         [notificationId, 'notificationInfo'],
-        { id: notificationId }
+        { id: notificationId, cordovaId }
       )
       .then(() => state.save(
         state.cloud.notifications,
@@ -20,7 +33,7 @@ export default /*@ngInject*/ function notifications(
     if (isDismissed(notificationId)) {
       return $q.when();
     }
-    
+
     return state.save(
       state.cloud.notifications,
       [notificationId, 'dismissed'],
