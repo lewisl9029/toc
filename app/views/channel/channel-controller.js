c10f76c348865854ee78539159eaa02835ac8cc8
Lewis Liu <develop@lewisl.net>
2015-09-10 10:56:59 -0700

    Removed obsolete comment

--- app/views/channel/channel-controller.js
+++ app/views/channel/channel-controller.js
@@ -6,9 +6,6 @@ export default /*@ngInject*/ function ChannelController(
   this.channelId = $stateParams.channelId;
 
   this.viewLatest = () => {
-    // FIXME: this actually scrolls ALL scroll views to bottom
-    // select a specific delegate instance using something similar to
-    // the approach in tocMessageList
     $ionicScrollDelegate.scrollBottom(true);
   };
 }
