aaf80afb9bd01680b316e8154c58a5c3ce278b59
Lewis Liu <develop@lewisl.net>
2015-09-18 01:26:43 -0700

    Removed redundant app abstract route

--- toc/app/views/channel/channel-config.js
+++ toc/app/views/channel/channel-config.js
@@ -2,7 +2,7 @@ import template from './channel.html!text';
 import controller, { controllerName } from './channel-controller';
 
 export default /*@ngInject*/ function configChannel($stateProvider) {
-  $stateProvider.state('app.private.channel', {
+  $stateProvider.state('private.channel', {
     url: '/channel/{channelId:string}',
     template: template,
     controller: controllerName + ' as channelView'
--- toc/app/views/welcome/welcome-config.js
+++ toc/app/views/welcome/welcome-config.js
@@ -2,7 +2,7 @@ import template from './welcome.html!text';
 import controller, { controllerName } from './welcome-controller';
 
 export default /*@ngInject*/ function configWelcome($stateProvider) {
-  $stateProvider.state('app.public.welcome', {
+  $stateProvider.state('public.welcome', {
     url: '/welcome',
     template: template,
     controller: controllerName + ' as welcomeView'
