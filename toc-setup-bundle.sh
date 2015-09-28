f19462b2ce1054a56d38b8a9f6958dff75b05649
Lewis Liu <develop@lewisl.net>
2015-09-28 02:51:15 -0700

    Bumping versions for dependencies

--- toc-setup-bundle.sh
+++ toc-setup-bundle.sh
@@ -7,10 +7,10 @@ if [ ! -f $BUNDLE_PATH/$TOC_CHROME_BUNDLE_NAME ]; then
     https://dl.google.com/linux/direct/$TOC_CHROME_BUNDLE_NAME --create-dirs -sS
 fi
 
-TOC_NODE_BUNDLE_NAME=node-v4.1.0-linux-x64.tar.gz
+TOC_NODE_BUNDLE_NAME=node-v4.1.1-linux-x64.tar.gz
 if [ ! -f $BUNDLE_PATH/$TOC_NODE_BUNDLE_NAME ]; then
   curl -o $BUNDLE_PATH/$TOC_NODE_BUNDLE_NAME \
-    https://nodejs.org/dist/v4.1.0/$TOC_NODE_BUNDLE_NAME --create-dirs -sS
+    https://nodejs.org/dist/v4.1.1/$TOC_NODE_BUNDLE_NAME --create-dirs -sS
 fi
 
 TOC_ANDROID_BUNDLE_NAME=android-sdk_r24.3.4-linux.tgz
