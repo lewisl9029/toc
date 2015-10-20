8c2dfe73bf08896ecfee27f807be67f09a0769a7
Lewis Liu <develop@lewisl.net>
2015-10-20 03:22:00 -0700

    Updated deploy script with user info

--- scripts/toc-deploy-staging.sh
+++ scripts/toc-deploy-staging.sh
@@ -8,6 +8,8 @@ mkdir -p toc-pages/releases/staging/$CIRCLE_BRANCH
 cp -r www/* toc-pages/releases/staging/$CIRCLE_BRANCH/
 
 cd toc-pages
+git config --global user.email "toc-deploy@lewisl.net"
+git config --global user.name "Lewis Liu"
 git add -A .
 git commit -m "Updated staging/$CIRCLE_BRANCH release"
 git push origin
