320d3404ad0052c8b0bbc6027539eb979860a4e1
Lewis Liu <develop@lewisl.net>
2015-10-20 01:57:31 -0700

    Implemented gh-pages deployment
    Added exceptions for gh-pages and toc-dev branches

--- scripts/toc-deploy-staging.sh
+++ scripts/toc-deploy-staging.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-gulp build
+git clone git@github.com:lewisl9029/toc.git toc-pages \
+  --branch gh-pages --depth 1
 
-git checkout gh-pages
-git symbolic-ref --short HEAD
+rm -rf toc-pages/releases/staging/$CIRCLE_BRANCH
+
+mkdir -p toc-pages/releases/staging/$CIRCLE_BRANCH
+cp -r www/* toc-pages/releases/staging/$CIRCLE_BRANCH/
+
+cd toc-pages
+git add -A .
+git commit -m "Updated staging/$CIRCLE_BRANCH release"
+git push origin
