a99042d14925e9852acf2c2233560ad8d0b12019
Lewis Liu <develop@lewisl.net>
2015-10-21 08:44:12 -0700

    Fixed max attempt detection in publish

--- scripts/toc-deploy-publish.sh
+++ scripts/toc-deploy-publish.sh
@@ -17,30 +17,20 @@ git push origin
 
 cd ..
 
-(
-cat <<LINK
-[Desktop Entry]
-Encoding=UTF-8
-Name=Toc Messenger
-Type=Link
-URL=${TOC_URL}
-Icon=text-html
-LINK
-) | tee $CIRCLE_ARTIFACTS/www.url
-
-
 MAX_ATTEMPTS=20
 current_attempt=1
-until ionic package download || [ $current_attempt == MAX_ATTEMPTS ];
+until ionic package download || [ $current_attempt == $MAX_ATTEMPTS ];
 do
   echo "Package download attempt failed. Trying again in 10 seconds."
   current_attempt=$((current_attempt+1))
   sleep 10
 done
 
-if current_attempt == MAX_ATTEMPTS;
+if [ $current_attempt == $MAX_ATTEMPTS ];
 then
   exit 1
 fi
 
 cp "Toc Messenger.apk" $CIRCLE_ARTIFACTS/
+
+echo $TOC_URL
