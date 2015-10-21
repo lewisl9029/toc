1014f19d7d18e2dfb134f7b07e7e1a427f66d356
Lewis Liu <develop@lewisl.net>
2015-10-21 12:46:19 -0700

    Moving apt install into dockerfile
    Updated docker dependencies

--- scripts/toc-install-deps.sh
+++ scripts/toc-install-deps.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-apt-get update && apt-get install -y \
-  git && \
-  apt-get clean && \
-  rm -rf /tmp/* /var/tmp/*
-
 npm install -g npm@3.3.8 && npm cache clean
 npm install -g gulp-cli@0.3.0 && npm cache clean
 npm install -g http-server@0.8.5 && npm cache clean
-npm install -g jspm@0.16.12 && npm cache clean
-npm install -g ionic@1.7.6 && npm cache clean
+npm install -g jspm@0.16.13 && npm cache clean
+npm install -g ionic@1.7.7 && npm cache clean
