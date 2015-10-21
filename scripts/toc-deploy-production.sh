45bcc56995fa528b0027bae7db48a9708b56fb49
Lewis Liu <develop@lewisl.net>
2015-10-21 13:53:33 -0700

    Added explicit tags push

--- scripts/toc-deploy-production.sh
+++ scripts/toc-deploy-production.sh
@@ -6,12 +6,12 @@ TOC_RELEASE=$TOC_VERSION
 TOC_URL="http://toc.im/releases/$TOC_RELEASE"
 
 git tag -d $TOC_VERSION
-git push origin :refs/tags/$TOC_VERSION
 
 # abort without error if version tag didn't change
 npm version $TOC_VERSION || exit 0
 
-git push origin
+git push origin :refs/tags/$TOC_VERSION
+git push origin --tags
 
 gulp package --prod
 
