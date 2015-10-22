970b84d86c8b57e87c1245b47e6b673f65248675
Lewis Liu <develop@lewisl.net>
2015-10-22 16:18:17 -0700

    Removed unused plugin references

--- toc/app/app-run.js
+++ toc/app/app-run.js
@@ -1,21 +1,8 @@
 export default /*@ngInject*/ function runApp(
   $ionicPlatform,
-  $window,
-  $log,
-  devices,
   session
 ) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default
-    // Remove this to show the accessory bar above the keyboard for form inputs
-    if ($window.cordova && $window.cordova.plugins.Keyboard) {
-      $window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if ($window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      $window.StatusBar.styleDefault();
-    }
-
     session.initialize();
   });
 }
