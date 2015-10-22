3168a6b6ee3dd5519c460ce18ae96173c4489bcf
Lewis Liu <develop@lewisl.net>
2015-10-22 16:05:12 -0700

    Removed old id scanner component

--- toc/app/components/id-scanner/id-scanner.js
+++
@@ -1,6 +0,0 @@
-import angular from 'angular';
-
-import directive, { directiveName } from './id-scanner-directive';
-
-export default angular.module('toc.components.id-scanner', [])
-  .directive(directiveName, directive);
