6b55c01bbe2aa5dd6d45c345c67496874ae47386
Lewis Liu <develop@lewisl.net>
2015-10-23 13:02:56 -0700

    Added current attempt count
    Switched line ending to lf

--- scripts/toc-download-package.sh
+++ scripts/toc-download-package.sh
@@ -3,14 +3,14 @@ set -e
 
 MAX_ATTEMPTS=15
 current_attempt=1
-until ionic package download || [ "$current_attempt" == "$MAX_ATTEMPTS" ];
+until ionic package download || [ "$current_attempt" == "$MAX_ATTEMPTS" ]
 do
-  echo "Package download attempt failed. Trying again in 10 seconds."
+  echo "Package download attempt $current_attempt failed. Trying again in 10 seconds."
   current_attempt=$((current_attempt+1))
   sleep 10
 done
 
-if [ "$current_attempt" == "$MAX_ATTEMPTS" ];
+if [ "$current_attempt" == "$MAX_ATTEMPTS" ]
 then
-  exit 1
+  exit 2
 fi
